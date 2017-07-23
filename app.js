2c49a87ce05f4bca03afa4cb25af6f221e6eca6e
sanlouise <sandra.adamshallie@gmail.com>
2017-07-23 12:21:46 -0400

    Load job title from indeed using cheerio

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const request = require('request');
 const fs = require('fs');
+const cheerio = require('cheerio');
 const app = express();
 const port = 8080;
+
+const url = "http://goo.gl/U6v4fE";
+
+request(url, (err, resp, body) => {
+  let $ = cheerio.load(body);
+  let jobTitle = $(".jobtitle font").text();
+  console.log(jobTitle);
+})
 
 app.listen(port, () => {
   console.log('app listening on port ' + port);
